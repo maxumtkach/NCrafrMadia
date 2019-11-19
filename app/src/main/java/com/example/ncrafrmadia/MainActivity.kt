512b061d02d65edf10bb66419a5aceca251615b3
maxumtkach <makstkach@bigmir.net>
2019-11-19 14:06:33 +0200

    NCraftMadia

--- app/src/main/java/com/example/ncrafrmadia/MainActivity.kt
+++ app/src/main/java/com/example/ncrafrmadia/MainActivity.kt
@@ -1,27 +1,61 @@
 package com.example.ncrafrmadia
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var countLike: Int = 0
+    private var countChat: Int = 0
+    private var countShare: Int = 0
+
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val post= Post(1,"Netology","First post in our network!","20 august  2019", true)
+        val post = Post(
+            "Netology", "First post in our network!",
+            "20 august  2019", false
+        )
 
-        post_text.text=post.content
+        post_text.text = post.content
+        netology_text.text = post.author
+        data_text.text = post.created
+    }
 
-        netology_text.text=post.author
-        data_text.text=post.created
-       // btn_image_like.setImageResource(R.drawable.ic_favorite_inactive_24dp)
-
-
-
-        if (post.likedByMe){
-            btn_image_like.setImageResource(R.drawable.ic_favorite_24dp)
+    private fun toColor() {
+        if (countLike != 0 || countChat != 0 || countShare != 0) {
+            post_text.setTextColor(Color.rgb(0, 0, 0))
         }
+    }
+
+    fun likedByMe(view: View) {
+
+        btn_image_like.setImageResource(R.drawable.ic_favorite_24dp)
+        countLike++
+        like_text.text = countLike.toString()
+        toColor()
+    }
+
+    fun chatClick(view: View) {
+
+        btn_image_chat.setImageResource(R.drawable.ic_chat_bubble_24dp)
+        countChat++
+        chat_text.text = countChat.toString()
+        toColor()
+    }
+
+    fun shareClick(view: View) {
+
+        share_btn.setImageResource(R.drawable.ic_share_24dp)
+        countShare++
+        share_text.text = countShare.toString()
+        toColor()
     }
 }
--- app/src/main/java/com/example/ncrafrmadia/post.kt
+++ app/src/main/java/com/example/ncrafrmadia/post.kt
@@ -1,16 +1,8 @@
 package com.example.ncrafrmadia
 
-import android.view.View
-
 class Post(
-    val id: Long,
     val author: String,
     val content: String,
     val created: String,  //data
     val likedByMe: Boolean = false
 )
-    fun countMe (view: View) {
-        // Get the value of the text view.
-    //    val countString = post.text.toString()
-
-}

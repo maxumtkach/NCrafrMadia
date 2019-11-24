a674c73eea31da1a56f284822f166f1c8fa00eb7
maxumtkach <makstkach@bigmir.net>
2019-11-24 10:28:55 +0200

    NCraftMadia

--- app/src/main/java/com/example/ncrafrmadia/MainActivity.kt
+++ app/src/main/java/com/example/ncrafrmadia/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.ncrafrmadia
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -29,18 +31,52 @@ class MainActivity : AppCompatActivity() {
         data_text.text = post.created
     }
 
-    private fun toColor() {
-        if (countLike != 0 || countChat != 0 || countShare != 0) {
-            post_text.setTextColor(Color.rgb(0, 0, 0))
+    fun locationByMe(view: View) {
+        val intent = Intent().apply {
+            //  data = Uri.parse("geo:$lat,$lng")
+            action = Intent.ACTION_VIEW
+            putExtra(
+                Intent.EXTRA_TEXT, """
+                ${post_text.text}(${netology_text.text})${data_text.text}
+            """.trimIndent()
+            )
+            type = "text/plain"
+        }
+        startActivity(intent)
+
+        //val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        // myToast.show()
+    }
+
+    private fun toColorDisactiv() {
+        if (countLike == 0) {
+            like_text.setTextColor(Color.rgb(255, 255, 255))
         }
     }
 
-    fun likedByMe(view: View) {
+    private fun toColorActiv() {
+        if (countLike != 0 || countChat != 0 || countShare != 0) {
+            like_text.setTextColor(Color.rgb(255, 0, 0))
+        }
+    }
 
-        btn_image_like.setImageResource(R.drawable.ic_favorite_24dp)
-        countLike++
-        like_text.text = countLike.toString()
-        toColor()
+    fun likedByMe(view: View) {     // функция лайк дизлайк
+
+        if (countLike != 0) {
+
+            countLike--
+            like_text.text = countLike.toString()
+
+            btn_image_like.setImageResource(R.drawable.ic_favorite_inactive_24dp)
+            toColorDisactiv()
+        } else {
+
+
+            btn_image_like.setImageResource(R.drawable.ic_favorite_red_24dp)
+            countLike++
+            like_text.text = countLike.toString()
+            toColorActiv()
+        }
     }
 
     fun chatClick(view: View) {
@@ -48,7 +84,8 @@ class MainActivity : AppCompatActivity() {
         btn_image_chat.setImageResource(R.drawable.ic_chat_bubble_24dp)
         countChat++
         chat_text.text = countChat.toString()
-        toColor()
+        toColorActiv()
+        toColorDisactiv()
     }
 
     fun shareClick(view: View) {
@@ -56,6 +93,7 @@ class MainActivity : AppCompatActivity() {
         share_btn.setImageResource(R.drawable.ic_share_24dp)
         countShare++
         share_text.text = countShare.toString()
-        toColor()
+        toColorActiv()
+        toColorDisactiv()
     }
 }
